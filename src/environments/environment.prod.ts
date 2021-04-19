a1c232fc835710b4d63b64f24b0e16814a01a151
Ezequiel Segota <ezequielsegota@hotmail.com>
2021-04-18 22:08:31 -0300

    Fix Heroku deploy...

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyBMCrMLNpVyP6zgu3cd-tgMgM_aJldcmrc",
+    authDomain: "sala-de-juegos-fb005.firebaseapp.com",
+    databaseURL: "https://sala-de-juegos-fb005-default-rtdb.firebaseio.com",
+    projectId: "sala-de-juegos-fb005",
+    storageBucket: "sala-de-juegos-fb005.appspot.com",
+    messagingSenderId: "847093944205",
+    appId: "1:847093944205:web:286467cc6101446e0337c4",
+    measurementId: "G-FCV18E298J"
+  }
 };
